be1136ae7053c9b4bbac5d39a50b89c771db81a8
Mark Rankilor <mark.rankilor@imgtec.com>
2013-10-07 14:23:23 +0100

    Remove dependence on project directory name of 'okra'.

--- samples/buildone.sh
+++ samples/buildone.sh
@@ -1,2 +1,2 @@
-g++ -g -I../../okra/dist/include -Isrc/cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -L../../okra/dist/bin -o ./dist/$1 src/cpp/$1/$1.cpp -lokra_x86_64
+g++ -g -I../dist/include -Isrc/cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -L../dist/bin -o ./dist/$1 src/cpp/$1/$1.cpp -lokra_x86_64
 cp src/cpp/$1/$1.hsail dist
