bdc62cdd97b27a6fb840b8a77ec049eb675fc41c
Eric Caspole <eric.caspole@amd.com>
2013-11-06 09:43:50 -0500

    revert buildone.sh

--- samples/buildone.sh
+++ samples/buildone.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-
-set -x
-
-g++ -g -I../dist/include -Isrc/cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -L../dist/bin -o ../dist/$1 src/cpp/$1/$1.cpp -lokra_x86_64 -ldl
-cp src/cpp/$1/*.hsail ../dist
+g++ -g -I../dist/include -Isrc/cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -L../dist/bin -o ./dist/$1 src/cpp/$1/$1.cpp -lokra_x86_64
+cp src/cpp/$1/$1.hsail dist
