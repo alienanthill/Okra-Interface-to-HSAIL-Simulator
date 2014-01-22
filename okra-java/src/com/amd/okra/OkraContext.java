4085d67abcf1614861e7dad11795c4584e011b51
Tom Deneau <tom.deneau@gmail.com>
2014-01-22 17:39:08 -0600

    separate temp dir loading into OkraSimLoader class, build separate jar for source files

--- okra-java/src/com/amd/okra/OkraContext.java
+++ okra-java/src/com/amd/okra/OkraContext.java
@@ -64,8 +64,14 @@ public class OkraContext {
         loadOkraNativeLibrary();
     } // end static
 
-	private static boolean isNativeLibraryLoaded;
-
+	static boolean isNativeLibraryLoaded;
+    private static String mappedOkraLibraryName;
+    public static String getMappedOkraLibraryName() {
+	    return mappedOkraLibraryName;
+    }
+    public static void setIsNativeLibraryLoaded( boolean b) {
+	    isNativeLibraryLoaded = b;
+    }
     static void loadOkraNativeLibrary() {
 		if (isNativeLibraryLoaded) return;
         String arch = System.getProperty("os.arch");
@@ -80,7 +86,7 @@ public class OkraContext {
             logger.fine("Expected property os.arch to contain amd64, x86_64, x86 or i386 but instead found " + arch + " as a result we don't know which okra to attempt to load.");
         }
 
-		String mappedOkraLibraryName = System.mapLibraryName(okraLibraryName);
+		mappedOkraLibraryName = System.mapLibraryName(okraLibraryName);
 		try {
 			if (okraLibraryName != null) {
 				// since the library directory (okra/dist/bin) also needs to be part of the PATH env variable, check that
@@ -114,109 +120,15 @@ public class OkraContext {
 		}
 
 		// and if we still haven't loaded it check if it is in the
-		// resources directory of our jar file.  If so, create a
+		// resources directory of a jar file.  If so, create a
 		// temporary directory and unjar it there.
-		String resourcesOkraDir = "resources/okra/";
-		Path tmpdirPath;
 		try {
-			tmpdirPath = Files.createTempDirectory("okrasim.dir_");
-			System.out.println("created temp directory" + tmpdirPath);
-			// if we couldn't even do that, give up
-			if (!tmpdirPath.toFile().exists()) {
-				throw new UnsatisfiedLinkError();
-			}
-			tmpdirPath.toFile().deleteOnExit();
-
-			copyResourcesToTmpdir(getResourcesList(resourcesOkraDir), resourcesOkraDir, tmpdirPath);
-	
-		} catch (IOException e) {
-            throw new RuntimeException("error creating temporary directory for jar resources");
-		}
-
-		// now load the library from the tmpdirPath
-		String okraLibraryFullName = tmpdirPath + File.separator + mappedOkraLibraryName;
-		if (new File(okraLibraryFullName).isFile()) {
-			System.load(okraLibraryFullName);
-			// System.out.println("loaded okra library from " + okraLibraryFullName);
-			isNativeLibraryLoaded = true;
-			return;
+		  Class.forName("com.amd.okra.OkraSimLoader", true, ClassLoader.getSystemClassLoader());
+		} catch (ClassNotFoundException e) {
+		  throw new RuntimeException(e.toString());
 		}
 	}
 
-
-	private static List<String> getResourcesList(String resourcesOkraDir) {
-		try {
-			// list files in a jar file resources directory
-			List<String> files = new ArrayList<String>();
-			
-			// Stream the jar file 
-			String jarpath = OkraContext.class.getResource("OkraContext.class").getPath();
-			// System.out.println("jar path is " + jarpath);
-			if (! jarpath.startsWith("file:")) {
-				throw new RuntimeException("Unable to get Jar input stream");
-			}
-			String jarfilename = jarpath.substring(5, jarpath.indexOf("!"));
-			// System.out.println("jarfilename is " + jarfilename );
-			JarInputStream jarInputStream = new JarInputStream(new FileInputStream(jarfilename));
-			JarEntry jarEntry;
-			
-			// Iterate the jar entries within that jar, looking for ones in resources/okra
-			do {
-				jarEntry = jarInputStream.getNextJarEntry();
-				if (jarEntry != null) {
-					String fileName = jarEntry.getName();
-					
-					// check against our filter
-					if (fileName.startsWith(resourcesOkraDir) && !fileName.equals(resourcesOkraDir)) {
-					    String root = fileName.substring(resourcesOkraDir.length());
-						files.add(root);
-					}
-				}
-			} while (jarEntry != null);
-
-			jarInputStream.close();
-			return files;
-		} catch (IOException e) {
-			throw new RuntimeException("error getting resource list from jar file");
-		}
-	}
-
-  private static void copyResourcesToTmpdir(List<String> fnames, String resourcesOkraDir, Path tmpDirPath) {
-		try {
-			// now for each file in resources folder copy to tmpdir
-			for (String fname : fnames) {
-				System.out.println("copying " + fname + " to tmpdir...");
-				// Prepare buffer for data copying
-				byte[] buffer = new byte[32768];
-				int readBytes;
-				
-				// Open and check input stream
-				InputStream myis = OkraContext.class.getResourceAsStream("/" + resourcesOkraDir + fname);
-				if (myis == null) {
-					throw new RuntimeException("File " + fname + " was not found inside jar:" + resourcesOkraDir);
-				}
-				
-				// Open output stream and copy data between source file in jar and the temporary file
-				File outfile = new File(tmpDirPath.toString() + File.separator + fname);
-				outfile.deleteOnExit();
-				try (OutputStream os = new FileOutputStream(outfile);
-					 InputStream is = myis) {
-						while ((readBytes = is.read(buffer)) != -1) {
-							os.write(buffer, 0, readBytes);
-						}
-					} catch (IOException e) {
-					throw new RuntimeException("error copying " + fname + " from jar file");
-				}
-				outfile.setExecutable(true, false);
-				outfile.setReadable(true, false);
-				if (!outfile.canExecute()) {
-					throw new RuntimeException("error setting execute on " + fname);
-				}
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("error copying resource from jar file to tmpdir");
-		}
-	}
 
     public boolean isValid() {
         return (contextHandle != 0);
---
+++ okra-java/src/com/amd/okra/OkraSimLoader.java
@@ -0,0 +1,170 @@
+// University of Illinois/NCSA
+// Open Source License
+// 
+// Copyright (c) 2013, Advanced Micro Devices, Inc.
+// All rights reserved.
+// 
+// Developed by:
+// 
+//     Runtimes Team
+// 
+//     Advanced Micro Devices, Inc
+// 
+//     www.amd.com
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal with
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimers.
+// 
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimers in the
+//       documentation and/or other materials provided with the distribution.
+// 
+//     * Neither the names of the LLVM Team, University of Illinois at
+//       Urbana-Champaign, nor the names of its contributors may be used to
+//       endorse or promote products derived from this Software without specific
+//       prior written permission.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+// SOFTWARE.
+//===----------------------------------------------------------------------===//
+package com.amd.okra;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.logging.*;
+import java.util.jar.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OkraSimLoader {
+
+  private static boolean verboseSimLoader = Boolean.getBoolean("okra.simloader.verbose");
+
+  static {
+	    String mappedOkraLibraryName = OkraContext.getMappedOkraLibraryName();
+		String resourcesOkraDir = "resources/okra/";
+		Path tmpdirPath;
+		try {
+			tmpdirPath = Files.createTempDirectory("okrasim.dir_");
+			if (verboseSimLoader) {
+			    System.out.println("created temp directory" + tmpdirPath);
+			}
+			// if we couldn't even do that, give up
+			if (!tmpdirPath.toFile().exists()) {
+				throw new UnsatisfiedLinkError();
+			}
+			tmpdirPath.toFile().deleteOnExit();
+
+			copyResourcesToTmpdir(getResourcesList(resourcesOkraDir), resourcesOkraDir, tmpdirPath);
+	
+		} catch (IOException e) {
+            throw new RuntimeException("error creating temporary directory for jar resources");
+		}
+
+		// now load the library from the tmpdirPath
+		String okraLibraryFullName = tmpdirPath + File.separator + mappedOkraLibraryName;
+		if (new File(okraLibraryFullName).isFile()) {
+			System.load(okraLibraryFullName);
+			// System.out.println("loaded okra library from " + okraLibraryFullName);
+			OkraContext.setIsNativeLibraryLoaded(true);
+		} else {
+			throw new RuntimeException("could not find " + mappedOkraLibraryName + " in jar file");
+		}
+    }
+
+
+	private static List<String> getResourcesList(String resourcesOkraDir) {
+		try {
+			// list files in a jar file resources directory
+			List<String> files = new ArrayList<String>();
+			
+			// Stream the jar file 
+			String jarpath = OkraSimLoader.class.getResource("OkraSimLoader.class").getPath();
+			// System.out.println("jar path is " + jarpath);
+			if (! jarpath.startsWith("file:")) {
+				throw new RuntimeException("Unable to get Jar input stream");
+			}
+			String jarfilename = jarpath.substring(5, jarpath.indexOf("!"));
+			// System.out.println("jarfilename is " + jarfilename );
+			JarInputStream jarInputStream = new JarInputStream(new FileInputStream(jarfilename));
+			JarEntry jarEntry;
+			
+			// Iterate the jar entries within that jar, looking for ones in resources/okra
+			do {
+				jarEntry = jarInputStream.getNextJarEntry();
+				if (jarEntry != null) {
+					String fileName = jarEntry.getName();
+					
+					// check against our filter
+					if (fileName.startsWith(resourcesOkraDir) && !fileName.equals(resourcesOkraDir)) {
+					    String root = fileName.substring(resourcesOkraDir.length());
+						files.add(root);
+					}
+				}
+			} while (jarEntry != null);
+
+			jarInputStream.close();
+			return files;
+		} catch (IOException e) {
+			throw new RuntimeException("error getting resource list from jar file");
+		}
+	}
+
+    private static void copyResourcesToTmpdir(List<String> fnames, String resourcesOkraDir, Path tmpDirPath) {
+		try {
+			// now for each file in resources folder copy to tmpdir
+			for (String fname : fnames) {
+			    if (verboseSimLoader) {
+				    System.out.println("copying " + fname + " to tmpdir...");
+				}
+				// Prepare buffer for data copying
+				byte[] buffer = new byte[32768];
+				int readBytes;
+				
+				// Open and check input stream
+				InputStream myis = OkraSimLoader.class.getResourceAsStream("/" + resourcesOkraDir + fname);
+				if (myis == null) {
+					throw new RuntimeException("File " + fname + " was not found inside jar:" + resourcesOkraDir);
+				}
+				
+				// Open output stream and copy data between source file in jar and the temporary file
+				File outfile = new File(tmpDirPath.toString() + File.separator + fname);
+				outfile.deleteOnExit();
+				try (OutputStream os = new FileOutputStream(outfile);
+					 InputStream is = myis) {
+						while ((readBytes = is.read(buffer)) != -1) {
+							os.write(buffer, 0, readBytes);
+						}
+					} catch (IOException e) {
+					throw new RuntimeException("error copying " + fname + " from jar file");
+				}
+				outfile.setExecutable(true, false);
+				outfile.setReadable(true, false);
+				if (!outfile.canExecute()) {
+					throw new RuntimeException("error setting execute on " + fname);
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("error copying resource from jar file to tmpdir");
+		}
+	}
+
+}
